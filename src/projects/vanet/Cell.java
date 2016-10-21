36e5700be124db5335f42d7537e9661db5120bf0
Cristopher Freitas <cristopherpk@gmail.com>
2016-10-21 17:04:41 -0300

    crrunning simulation

---
+++ src/projects/vanet/Cell.java
@@ -0,0 +1,16 @@
+package projects.vanet;
+
+import jsensor.nodes.monitoring.DefaultJsensorCell;
+
+public class Cell extends DefaultJsensorCell {
+
+	public Cell() {
+		super();
+	}
+	
+	@Override
+	public DefaultJsensorCell clone(){
+		return new Cell();
+	}
+	
+}
